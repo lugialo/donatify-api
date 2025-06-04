291575bd124348be9d732bb03b0e3555eeb682d3
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2025-06-03 22:56:07 -0300

    feat: First commit

---
+++ src/main/java/com/lugialo/donatify/DonatifyApplication.java
@@ -0,0 +1,13 @@
+package com.lugialo.donatify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lugialo/donatify/DonatifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugialo.donatify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
