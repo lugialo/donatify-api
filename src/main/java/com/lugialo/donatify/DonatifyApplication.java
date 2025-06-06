6ec63ebf2a8acd725f12ee61916102775803bd5c
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2025-06-06 09:23:58 -0300

    Remoção do .env no run da aplicação

--- src/main/java/com/lugialo/donatify/DonatifyApplication.java
+++ src/main/java/com/lugialo/donatify/DonatifyApplication.java
@@ -1,6 +1,5 @@
 package com.lugialo.donatify;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DonatifyApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
 		SpringApplication.run(DonatifyApplication.class, args
 		);
 	}
